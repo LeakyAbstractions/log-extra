198dfc3e456ca6f778c94bc58d0ed95d470a4475
Guillermo Calvo <guillermocalvo@yahoo.com>
2017-11-07 23:19:10 +0900

    Add initial version of the plain layout

---
+++ src/layout/l_plain.c
@@ -0,0 +1,25 @@
+/**
+ *
+ * @file        l_plain.c
+ *
+ * Plain layout source code file
+ *
+ * @version     1.0.0
+ * @author      Copyright (c) 2017 Guillermo Calvo
+ *
+ */
+
+# include "l_plain.h"
+
+# include <stdio.h>
+# include <string.h>
+
+static int format(struct log_layout * layout, struct fmt_stream * stream, struct log_logger * logger, struct log_event * event){
+
+    (void)layout;
+    (void)logger;
+
+    return( fmt_vprint(stream, event->format, event->arg) );
+}
+
+struct log_layout_type log_plain_layout = { NULL, NULL, format };
---
+++ src/layout/l_plain.h
@@ -0,0 +1,27 @@
+/**
+ *
+ * @file        l_plain.h
+ *
+ * Plain layout header file
+ *
+ * @version     1.0.0
+ * @author      Copyright (c) 2017 Guillermo Calvo
+ *
+ */
+
+# ifndef LOG_PLAIN_LAYOUT_H
+# define LOG_PLAIN_LAYOUT_H
+
+# include <log.h>
+
+/** Plain layout type */
+extern struct log_layout_type log_plain_layout;
+
+/** Plain layout configuration */
+struct log_plain_layout_config{
+
+    /** Unused field */
+    char _;
+};
+
+# endif
